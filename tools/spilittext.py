99082c082142d6570f6134467fdf801d454136c8
unknown <1126220529@qq.com>
2024-02-28 14:19:24 +0800

    [ADD] 文字分割

--- tools/spilittext.py
+++ tools/spilittext.py
@@ -1,15 +1,36 @@
+import os
 
 TrainTextFile = '西游记_白话文版.txt'
+FILE_RESULT = 'split'
+
 
 def amain():
     splitText('&#8195;;')
 
+
+def getChildFileName(file_name, index):
+    return f"{FILE_RESULT}/{getSomeIndex(file_name, index)}.txt"
+
+
+def getSomeIndex(file_name, index):
+    return f"第{index}集  {file_name}"
+
+
 def splitText(splitSimp):
+    if not os.path.exists(FILE_RESULT):
+        os.makedirs(FILE_RESULT)
 
     with open(TrainTextFile, "r", encoding='utf-8') as text_file:
         text = text_file.read()
-        splitted = text.split(splitSimp)
+        splitteds = text.split(splitSimp)
+        # 获取文件名
+        file_name, ext = os.path.splitext(TrainTextFile)
 
+        for i in range(len(splitteds)):
+            with open(getChildFileName(file_name, i), "w", encoding="utf-8") as file:
+                file.write(getSomeIndex(file_name, i))
+                file.write("\n")
+                file.write(splitteds[i].strip())
 
 
 if __name__ == "__main__":
